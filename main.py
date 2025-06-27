3d847bfa416574e99150f470ca6c6f9b07b2d788
danipoav <dapoav2002@gmail.com>
2025-06-27 16:57:59 +0200

    Finishing project

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    todas_las_tareas = db.session.query(Tarea).all()
+    return render_template("index.html", lista_de_tareas=todas_las_tareas)
 
 
 @app.route('/crear-tarea', methods=['POST'])
@@ -15,7 +16,22 @@ def crear():
     tarea = Tarea(contenido=request.form['contenido_tarea'], hecha=False)
     db.session.add(tarea)
     db.session.commit()
-    return "Tarea guardada"
+    return redirect(url_for('home'))
+
+
+@app.route('/eliminar-tarea/<id>')
+def eliminar(id):
+    tarea = db.session.query(Tarea).filter_by(id=int(id)).delete()
+    db.session.commit()
+    return redirect(url_for('home'))
+
+
+@app.route('/tarea-hecha/<id>')
+def hecha(id):
+    tarea = db.session.query(Tarea).filter_by(id=int(id)).first()
+    tarea.hecha = not (tarea.hecha)
+    db.session.commit()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
